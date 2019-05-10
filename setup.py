40de22c904cb95728ac2d36dea47fe4c158b25ea
Hans Lee <hans.lee@kakaobrain.com>
2019-05-10 17:14:31 +0900

    Prepare setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+about = {}  # type: ignore
+with open('torchgpipe/__about__.py') as f:
+    exec(f.read(), about)  # pylint: disable=W0122
+
+
+setup(
+    name='torchgpipe',
+    version=about['__version__'],
+    author='Kakao Brain',
+    maintainer='Heungsub Lee, Myungryong Jeong',
+    zip_safe=False,
+    packages=['torchgpipe'],
+    install_requires=['torch'],
+)
