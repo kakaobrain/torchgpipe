904e864f89ffb6f0d844753ae88229da1ec58b4a
Heungsub Hans Lee <hans.lee@kakaobrain.com>
2019-06-18 13:32:30 +0900

    Test balance_by_time

--- tests/test_balancing.py
+++ tests/test_balancing.py
@@ -1,6 +1,10 @@
-import pytest
+import time
 
-from torchgpipe_balancing import blockpartition
+import pytest
+import torch
+from torch import nn
+
+from torchgpipe_balancing import balance_by_time, blockpartition
 
 
 def test_blockpartition():
@@ -23,3 +27,19 @@ def test_blockpartition_short_sequence():
         blockpartition.solve([], partitions=1)
     with pytest.raises(ValueError):
         blockpartition.solve([42], partitions=2)
+
+
+def test_balance_by_time():
+    class Delay(nn.Module):
+        def __init__(self, seconds):
+            super().__init__()
+            self.seconds = seconds
+
+        def forward(self, x):
+            time.sleep(self.seconds)
+            return x
+
+    model = nn.Sequential(*[Delay(i/100) for i in [1, 2, 3, 4, 5, 6]])
+    sample = torch.rand(1)
+    balance = balance_by_time(model, sample, partitions=2, device='cpu')
+    assert balance == [4, 2]
