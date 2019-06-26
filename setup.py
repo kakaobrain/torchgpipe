9f2658d2712936becc42aa0d098335212498d5de
Heungsub Hans Lee <hans.lee@kakaobrain.com>
2019-06-26 14:34:45 +0900

    Write long description in PyPI

--- setup.py
+++ setup.py
@@ -1,3 +1,45 @@
+"""
+torchgpipe
+==========
+
+A GPipe_ implementation in PyTorch_.
+
+.. _GPipe: https://arxiv.org/abs/1811.06965
+.. _PyTorch: https://pytorch.org/
+
+.. sourcecode:: python
+
+   from torchgpipe import GPipe
+
+   model = nn.Sequential(a, b, c, d)
+   model = GPipe(model, balance=[1, 1, 1, 1], chunks=8)
+
+   for input in data_loader:
+       output = model(input)
+
+What is GPipe?
+~~~~~~~~~~~~~~
+
+GPipe is a scalable pipeline parallelism library published by Google Brain,
+which allows efficient training of large, memory-consuming models. According to
+the paper, GPipe can train a 25x larger model by using 8x devices (TPU), and
+train a model 3.5x faster by using 4x devices.
+
+`GPipe: Efficient Training of Giant Neural Networks using Pipeline Parallelism
+<https://arxiv.org/abs/1811.06965>`_
+
+Google trained AmoebaNet-B with 557M parameters over GPipe. This model has
+achieved 84.3% top-1 and 97.0% top-5 accuracy on ImageNet classification
+benchmark (the state-of-the-art performance as of May 2019).
+
+Links
+~~~~~
+
+- Source Code: https://github.com/kakaobrain/torchgpipe
+- Documentation: https://torchgpipe.readthedocs.io/
+- Original Paper: https://arxiv.org/abs/1811.06965
+
+"""
 from setuptools import setup
 
 
@@ -19,6 +61,7 @@ setup(
     maintainer='Heungsub Lee, Myungryong Jeong',
 
     description='GPipe for PyTorch',
+    long_description=__doc__,
     keywords='pytorch gpipe',
 
     zip_safe=False,
