3379788a8891ed83dea96f5097029923ea9e419d
Heungsub Hans Lee <heungsub.lee@kakaobrain.com>
2019-09-23 21:13:32 +0900

    Move test_identicalness into test_transparency

--- tests/test_gpipe.py
+++ tests/test_gpipe.py
@@ -85,44 +85,6 @@ def test_too_few_devices():
         model = GPipe(model, balance=[1, 1, 1, 1], devices=['cpu'])
 
 
-def test_identicalness():
-    def sum_grad(parameters):
-        return sum([p.grad.sum() for p in parameters if p.grad is not None])
-
-    def zero_grad(parameters):
-        for p in parameters:
-            p.grad = None
-
-    inputs = torch.rand(8, 1)
-    model = nn.Sequential(
-        nn.Linear(1, 2),
-        nn.Linear(2, 4),
-        nn.Linear(4, 2),
-        nn.Linear(2, 1),
-    )
-
-    # Without GPipe
-    outputs = model(inputs)
-    loss = outputs.mean()
-    loss.backward()
-
-    grad_without_gpipe = sum_grad(model.parameters())
-
-    zero_grad(model.parameters())
-
-    # With GPipe
-    model = GPipe(model, [2, 2], devices=['cpu', 'cpu'], chunks=4)
-
-    outputs = model(inputs)
-    loss = outputs.mean()
-    loss.backward()
-
-    grad_with_gpipe = sum_grad(model.parameters())
-
-    # Both grads should be identical.
-    assert torch.allclose(grad_with_gpipe, grad_without_gpipe)
-
-
 def test_batch_size_indivisible():
     model = nn.Sequential(nn.Linear(1, 1))
     model = GPipe(model, balance=[1], devices=['cpu'], chunks=4)
