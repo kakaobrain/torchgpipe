c6da423f0020e8ad71141c4deef4af64cdf08707
Heungsub Hans Lee <heungsub.lee@kakaobrain.com>
2019-09-23 21:06:24 +0900

    Test forward lockstep in test_pipeline

---
+++ tests/test_pipeline.py
@@ -0,0 +1,40 @@
+import time
+
+import torch
+import torch.nn as nn
+
+from torchgpipe.microbatch import Batch
+from torchgpipe.pipeline import Pipeline
+
+
+def test_forward_lockstep():
+    timeline = []
+
+    class DelayedLog(nn.Module):
+        def __init__(self, i, seconds):
+            super().__init__()
+            self.i = i
+            self.j = 0
+            self.seconds = seconds
+
+        def forward(self, x):
+            time.sleep(self.seconds)
+
+            timeline.append((self.i, self.j))
+            self.j += 1
+
+            return x
+
+    batches = [Batch(torch.rand(1, 1)) for _ in range(3)]
+    partitions = [nn.Sequential(DelayedLog(0, seconds=0)),
+                  nn.Sequential(DelayedLog(1, seconds=0.1))]
+
+    pipeline = Pipeline(batches, partitions)
+    pipeline.run()
+
+    # Expected timeline: (Logs are recorded at !)
+    #
+    # Partition #0: 0! 1!   2!
+    # Partition #1:    000! 111! 222!
+    #
+    assert timeline == [(0, 0), (0, 1), (1, 0), (0, 2), (1, 1), (1, 2)]
