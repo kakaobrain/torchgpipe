3ad067044552c84e51f56b7b253f79f5d948c888
Heungsub Hans Lee <heungsub.lee@kakaobrain.com>
2019-11-20 21:51:12 +0900

    Explain why namespaces are ordered by UUID-4

--- torchgpipe/skip/namespace.py
+++ torchgpipe/skip/namespace.py
@@ -42,12 +42,18 @@ class Namespace(metaclass=abc.ABCMeta):
     __slots__ = ('id',)
 
     def __init__(self) -> None:
-        # Version 4 UUID makes a namespace able to be copied or resotred.
         self.id = uuid.uuid4()
+
+    def __repr__(self) -> str:
+        return f"<Namespace '{self.id}'>"
 
     def __hash__(self) -> int:
         return hash(self.id)
 
+    # Namespaces should support ordering, since SkipLayout will sort tuples
+    # including a namespace. But actual order between namespaces is not
+    # necessary. That's why they are ordered by version 4 UUID which generates
+    # random numbers.
     def __lt__(self, other: Any) -> bool:
         if isinstance(other, Namespace):
             return self.id < other.id
@@ -57,9 +63,6 @@ class Namespace(metaclass=abc.ABCMeta):
         if isinstance(other, Namespace):
             return self.id == other.id
         return False
-
-    def __repr__(self) -> str:
-        return f"<Namespace '{self.id}'>"
 
 
 # 'None' is the default namespace,
