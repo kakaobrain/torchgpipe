9fac75eb003aa157c95324514116d0baea03ddf6
Heungsub Hans Lee <heungsub.lee@kakaobrain.com>
2019-11-27 17:33:47 +0900

    Remove meaningless test_skip_leak

--- tests/skip/test_leak.py
+++ tests/skip/test_leak.py
@@ -23,56 +23,6 @@ class Pop(nn.Module):
         return input + skip
 
 
-@pytest.fixture
-def count_leaked_portals(monkeypatch):
-    """This fixture is a function to count leaked
-    :class:`torchgpipe.skip.portal.Portal` objects.
-    """
-    skip_trackers = WeakSet()
-
-    def count():
-        counter = 0
-        for skip_tracker in skip_trackers:
-            counter += len([p for p in skip_tracker.portals if not (p.tensor is p.grad is None)])
-            counter += len([x for x in skip_tracker.tensors if x is not None])
-        return counter
-
-    def _current_skip_tracker(orig=current_skip_tracker):
-        skip_tracker = orig()
-        skip_trackers.add(skip_tracker)
-        return skip_tracker
-    monkeypatch.setattr('torchgpipe.skip.tracker.current_skip_tracker', _current_skip_tracker)
-
-    return count
-
-
-@pytest.mark.parametrize('train', [True, False], ids=['train', 'eval'])
-@pytest.mark.parametrize('gpipe, checkpoint',
-                         [(False, None), (True, 'always'), (True, 'never')],
-                         ids=['bare', 'gpipe[never]', 'gpipe[always]'])
-def test_skip_leak(count_leaked_portals, train, gpipe, checkpoint, monkeypatch):
-    model = nn.Sequential(Stash(), Pop())
-
-    if gpipe:
-        model = GPipe(model,
-                      balance=[1, 1],
-                      devices=['cpu', 'cpu'],
-                      chunks=2,
-                      checkpoint=checkpoint)
-
-    input = torch.rand(10, requires_grad=True)
-
-    if train:
-        model.train()
-        model(input).norm().backward()
-    else:
-        model.eval()
-        with torch.no_grad():
-            model(input)
-
-    assert count_leaked_portals() == 0
-
-
 @pytest.mark.parametrize('train', [True, False], ids=['train', 'eval'])
 @pytest.mark.parametrize('checkpoint', ['always', 'except_last', 'never'])
 def test_delete_portal_tensor(train, checkpoint):
